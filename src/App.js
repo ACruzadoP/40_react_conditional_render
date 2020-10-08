dae7b49ae0468b6f331cb0c1968192de0808fa7e
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 04:58:54 +0200

    Tomando snapshot de un ejercicio terminado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import Conditional from "./Conditional"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoading: true
+        }
+    }
+    
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 1500)
+    }
+    
+    // condition ? 
+    // statement if true : 
+    // statement if false
+
+    render() {
+        return (
+            <div>
+                {this.state.isLoading ?
+                <h1>Loading...</h1> :
+                <Conditional />}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/Conditional.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Conditional () {
+    return (
+        <h1>Already Loaded! :)</h1>
+    )
+}
+
+export default Conditional
